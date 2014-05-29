1dd0cb500da77b3cbd433aab7fbc4abdbde8e828
Chris Gibson <chris.david.gibson@gmail.com>
2014-05-29 15:00:14 -0500

    Removing redundant string check when returning cached values

--- src/TelQ.js
+++ src/TelQ.js
@@ -9,94 +9,86 @@ var cacheTime = (60 * 1000); //cache specified in minutes
 var telQCachingEnabled = true;
 
 function TelQ() {
-    'use strict';
+  'use strict';
 
-    function get(url, options) {
-        options.expires = (options.expires || 0) * cacheTime;
+  function get(url, options) {
+    options.expires = (options.expires || 0) * cacheTime;
 
-        function qGetHttp(resolve, reject) {
+    function qGetHttp(resolve, reject) {
 
-            function requestCallback(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var result = typeof body ==='string' ? JSON.parse(body) : body;
+      function requestCallback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var result = typeof body === 'string' ? JSON.parse(body) : body;
 
-                    if (telQCachingEnabled && options.expires > 0) {
-                        cache.add({
-                            id: url,
-                            value: [result, response],
-                            expires: new Date(new Date().getTime() + options.expires)
-                        });
-                    }
+          if (telQCachingEnabled && options.expires > 0) {
+            cache.add({
+              id: url,
+              value: [result, response],
+              expires: new Date(new Date().getTime() + options.expires)
+            });
+          }
 
-                    resolve(result, response);
-                } else {
-                    reject(error);
-                }
-            }
-
-            function returnIfCached(cachedItem) {
-                if (cachedItem.id === url) {
-                  var result = (typeof cachedItem.value[0] === 'string') ?
-                        JSON.parse(cachedItem.value[0]) :
-                        cachedItem.value[0];
-
-                    var response = (typeof cachedItem.value[1] === 'string') ?
-                        JSON.parse(cachedItem.value[1]) :
-                        cachedItem.value[1];
-
-                    resolve(result, response);
-                }
-            }
-
-
-            options.params = options.params ? reOrder(options.params) : null;
-
-            url = (options.params) ?
-                url + '?' + qs.stringify(options.params) :
-                url;
-
-            //memoize short circuit.
-            if (telQCachingEnabled) {
-                _.each(cache.list(), returnIfCached);
-            }
-
-            request(url, requestCallback);
+          resolve(result, response);
+        } else {
+          reject(error);
         }
-
-        return new RSVP.Promise(qGetHttp);
-    }
-
-    function post(options) {
-        function qGetHttp(resolve, reject) {
-            function requestCallback(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    resolve(body, response);
-                } else {
-                    reject(error);
-                }
-            }
-            request.post(options, requestCallback);
-
-        }
-        return new RSVP.Promise(qGetHttp);
-    }
-
-    function use(option) {
-      if(option && typeof option === 'object') {
-        q = _.assign(q, option);
-      } else {
-        throw 'Option not an object';
       }
+
+      function returnIfCached(cachedItem) {
+        if (cachedItem.id === url) {
+          resolve(cachedItem.value[0], cachedItem.value[1]);
+        }
+      }
+
+
+      options.params = options.params ? reOrder(options.params) : null;
+
+      url = (options.params) ?
+        url + '?' + qs.stringify(options.params) :
+        url;
+
+      //memoize short circuit.
+      if (telQCachingEnabled) {
+        _.each(cache.list(), returnIfCached);
+      }
+
+      request(url, requestCallback);
     }
 
-    var q = _.assign(this, RSVP);
+    return new RSVP.Promise(qGetHttp);
+  }
 
-    //RSVP.configure('onerror', function(reason){ console.assert(false,reason)}); //Error handling?
+  function post(options) {
+    function qGetHttp(resolve, reject) {
+      function requestCallback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          resolve(body, response);
+        } else {
+          reject(error);
+        }
+      }
+      request.post(options, requestCallback);
 
-    q.use = use;
-    q.post = post;
-    q.get = get;
-    return q;
+    }
+    return new RSVP.Promise(qGetHttp);
+  }
+
+  function use(option) {
+    if (option && typeof option === 'object') {
+      q = _.assign(q, option);
+    } else {
+      throw 'Option not an object';
+    }
+  }
+
+  var q = _.assign(this, RSVP);
+
+  //RSVP.configure('onerror', function(reason){ console.assert(false,reason)}); //Error handling?
+
+  q.use = use;
+  q.post = post;
+  q.get = get;
+  return q;
 }
 
 module.exports = new TelQ();
