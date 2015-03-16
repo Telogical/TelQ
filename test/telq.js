b8ccd5018f2850c147430d636baecea414b57772
Joe Hare <joseph.hare@gmail.com>
2015-03-16 13:32:11 -0500

    Fix unit test and use defer instead of promise

--- test/telq.js
+++ test/telq.js
@@ -53,13 +53,18 @@ describe('Given I want to make an asynchronous request for a resource', function
 
             describe('When I submit a GET request to the resource', function () {
 
-                it('Then I should receive data from the resource', function (done) {
+                it('Then I should receive data from the resource rabbits', function (done) {
                     var options = {
                         source: server + resource
                     };
+                    
+                    var expectedResult = {
+                      'datum': 'some data'
+                    };
+
                     var qUrl = q.get(options);
 
-                    expect(qUrl).to.eventually.deep.equal(result).and.notify(done);
+                    expect(qUrl).to.eventually.deep.equal(expectedResult).and.notify(done);
                 });
 
             });
