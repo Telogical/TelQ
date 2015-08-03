228f7087c8d68409ff913ef0da502c7139c92d67
Chris <chris.david.gibson@gmail.com>
2015-08-02 21:34:08 -0500

    Remove nock and use sinon (closes #64)
      * Remove nock from tests
      * Use sinon to stub request.get and request.post
      * Remove reOrder.js that was written specifically for nock

--- test/telq.js
+++ test/telq.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var _ = require('lodash');
+var sinon = require('sinon');
+var request = require('request');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
@@ -8,117 +10,134 @@ var expect = chai.expect;
 var q = require('./../src/TelQ.js');
 
 describe('Given I want to use TelQ', function () {
-    describe('When I initialize TelQ', function () {
+  describe('When I initialize TelQ', function () {
 
-        it('Then it should have a get function by default', function (done) {
-            var hasGet = _.has(q, 'get');
-            expect(hasGet).to.equal(true);
-            done();
-        });
-
-        it('Then it should have a post function by default', function (done) {
-            var hasPost = _.has(q, 'post');
-            expect(hasPost).to.equal(true);
-            done();
-        });
+    it('Then it should have a get function by default', function (done) {
+      var hasGet = _.has(q, 'get');
+      expect(hasGet).to.equal(true);
+      done();
     });
+
+    it('Then it should have a post function by default', function (done) {
+      var hasPost = _.has(q, 'post');
+      expect(hasPost).to.equal(true);
+      done();
+    });
+  });
 });
 
 describe('Given I want to make an asynchronous request for a resource', function () {
 
-    describe('And that resource is a rest based url', function () {
+  describe('And that resource is a rest based url', function () {
+    var server = 'http://server';
+    var resource = '/resource';
+    var qUrl;
 
-        var server = 'http://server';
-        var resource = '/resource';
+    describe('And the resource returns without an error', function () {
+      var result = {
+        'datum': 'some data'
+      };
 
-        describe('And the resource returns without an error', function () {
-            var nock = require('nock');
-            var result = {
-                'datum': 'some data'
-            };
+      beforeEach(function () {
+        sinon.stub(request, 'get', function(url, callback) {
+          callback(null, {statusCode: 200}, result)
+        });
+        
+        sinon.stub(request, 'post', function(url, callback) {
+          callback(null, {statusCode: 200}, result)
+        });
+      });
 
-            beforeEach(function () {
-                nock(server).get(resource).delay(10).reply(200, result, {
-                    'Content-Type': 'application/json'
-                });
-                nock(server).post(resource).delay(10).reply(200, result, {
-                    'Content-Type': 'application/json'
-                });
-            });
-
-            afterEach(function () {
-                nock.cleanAll();
-            });
+      afterEach(function () {
+        request.get.restore();
+        request.post.restore();
+      });
 
 
-            describe('When I submit a GET request to the resource', function () {
+      describe('When I submit a GET request to the resource', function () {
 
-                it('Then I should receive data from the resource', function (done) {
-                    var options = {
-                        source: server + resource
-                    };
-                    
-                    var expectedResult = {
-                      'datum': 'some data'
-                    };
+        beforeEach(function() {
+          var options = {
+            source: server + resource
+          };
 
-                    var qUrl = q.get(options);
-
-                    expect(qUrl).to.eventually.deep.equal(expectedResult).and.notify(done);
-                });
-
-            });
-
-            describe('When I submit a POST to the resource', function () {
-
-                it('Then I should receive data from the resource', function (done) {
-                    var options = {
-                        source: server + resource
-                    };
-                    var qUrl = q.post(options);
-
-                    expect(qUrl).to.eventually.be.fulfilled.and.notify(done);
-                });
-
-            });
+          qUrl = q.get(options);
         });
 
-        describe('And the resource returns with an error', function () {
-            var nock = require('nock');
-            var result = 'some error';
+        it('Then I should receive data from the resource', function (done) {
+          var expectedResult = {
+            'datum': 'some data'
+          };
 
-            beforeEach(function () {
-                nock(server).get(resource).delay(10).reply(500, result);
-                nock(server).post(resource).delay(10).reply(500, result);
-            });
-
-            afterEach(function () {
-                nock.cleanAll();
-            });
-
-            describe('When I submit a GET request to the resource', function () {
-
-                it('Then I should receive a rejection from the resource', function (done) {
-                    var options = {
-                        source: server + resource
-                    };
-                    var qUrl = q.get(options);
-
-                    expect(qUrl).to.eventually.be.rejected.and.notify(done);
-                });
-            });
-
-            describe('When I submit a POST to the resource', function () {
-
-                it('Then I should receive a rejection from the resource', function (done) {
-                    var options = {
-                        source: server + resource
-                    };
-                    var qUrl = q.post(options);
-
-                    expect(qUrl).to.eventually.be.rejected.and.notify(done);
-                });
-            });
+          expect(qUrl).to.eventually.deep.equal(expectedResult).and.notify(done);
         });
+
+      });
+
+      describe('When I submit a POST to the resource', function () {
+        
+        beforeEach(function() {
+          var options = {
+            source: server + resource
+          };
+
+          qUrl = q.post(options);
+        });
+
+        it('Then I should receive data from the resource', function (done) {
+          expect(qUrl).to.eventually.be.fulfilled.and.notify(done);
+        });
+
+      });
     });
+
+    describe('And the resource returns with an error', function () {
+      var result = 'some error';
+
+      beforeEach(function () {
+        sinon.stub(request, 'get', function(url, callback) {
+          callback(true, {statusCode: 404}, result)
+        });
+        
+        sinon.stub(request, 'post', function(url, callback) {
+          callback(true, {statusCode: 404}, result)
+        });
+      });
+
+      afterEach(function () {
+        request.get.restore();
+        request.post.restore();
+      });
+
+      describe('When I submit a GET request to the resource', function () {
+
+        beforeEach(function() {
+          var options = {
+            source: server + resource
+          };
+
+          qUrl = q.post(options);
+        });
+
+        it('Then I should receive a rejection from the resource', function (done) {
+          expect(qUrl).to.eventually.be.rejected.and.notify(done);
+        });
+      });
+
+      describe('When I submit a POST to the resource', function () {
+
+        beforeEach(function() {
+          var options = {
+            source: server + resource
+          };
+
+          qUrl = q.post(options);
+        });
+
+        it('Then I should receive a rejection from the resource', function (done) {
+          expect(qUrl).to.eventually.be.rejected.and.notify(done);
+        });
+      });
+    });
+  });
 });
